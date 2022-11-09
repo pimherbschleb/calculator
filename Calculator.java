6b2f75ddecc4710c06118ac41732b46d6461792a
pim <pimnelson@hotmail.com>
2022-11-09 16:01:19 +0100

    small change addition

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,8 @@
 public class calculator {
     public int addition(int a, int b){
-        return a + b
+        return a + b;
     }
     public doub;e addition(int a, int b){
-        return a + b
+        return a + b;
     }
 }
