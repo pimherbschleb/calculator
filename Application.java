16cff5f7d339ecd5b58104a989cb7bbb049a9dcd
pim <pimnelson@hotmail.com>
2022-11-09 14:35:18 +0100

    added application to our calculator project

---
+++ Application.java
@@ -0,0 +1,7 @@
+public class Application{
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.printIn(calculator.addition(1,2)); // should be 3
+        System.out.printIn(calculator.addition(1.5,3.2)); // should be 4.7
+    }
+}
