b11f8abcc1b01608995a930c4a4ad16ef530e96b
collaborator <pimnelson@hotmail.com>
2022-12-16 13:57:17 +0100

    added multiplication to the calculator

--- Calculator.java
+++ Calculator.java
@@ -6,4 +6,10 @@ public class calculator {
     public doub;e addition(int a, int b){
         return a + b;
     }
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    public double mutiply(double a, double b) {
+        return a * b;
+    }
 }
